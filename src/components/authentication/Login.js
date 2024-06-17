d46ca75ab85526c08ee323fca1486804d0b5702e
kulraj chavda <kulraj710@gmail.com>
2024-06-17 23:00:37 +0530

    [refactor] : add demo login details on login page

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -32,6 +32,12 @@ export default function Login() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Log In</h2>
+          {/* for demo */}
+          <pre>
+            Demo Email : test@test.com
+            <br/>
+            Password : test123
+          </pre>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
