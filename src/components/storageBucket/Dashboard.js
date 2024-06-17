801ae102ec1b5732fddc8c9b1640677a7197b5de
kulraj chavda <kulraj710@gmail.com>
2024-06-17 23:01:08 +0530

    [feat] : add how to use banner in dashboard

--- src/components/storageBucket/Dashboard.js
+++ src/components/storageBucket/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { Container } from "react-bootstrap"
 import { useFolder } from "../../hooks/useFolder"
 import AddFolderButton from "./AddFolderButton"
@@ -14,10 +14,34 @@ export default function Dashboard() {
   const { state = {} } = useLocation()
   const { folder, childFolders, childFiles } = useFolder(folderId, state.folder)
 
+  const [showNotice, setShowNotice] = useState(true);
+
+  const hideNotice = () => {
+    setShowNotice(false) 
+  }
+
+  const styles = {
+    margin : '3rem',
+    padding : '1rem',
+    backgroundColor : '#f5f5f5',
+    border : '0.7px solid lightgrey',
+    borderRadius : '10px'
+  }
   return (
     <>
       <Navbar />
       <Container fluid>
+        {/* Note to User, Remove Later */}
+        {showNotice ? <div style={styles}>
+          <h5>How to Use? <button onClick={hideNotice}>Hide This</button></h5>
+          <p>This is your Dashboard, click on Icons to the right to Upload new file to the drive or create new folder to organize your files.</p>
+          <p>TO-DO : 
+          <br/>
+          [feat] Preview option, currently user has to rely on browser to view their file. Implement preview for files just like available in Google Drive.</p>
+          <p>
+            [feat] Display Metadata
+          </p> 
+        </div> : null }
         <div className="d-flex align-items-center">
           <FolderBreadcrumbs currentFolder={folder} />
           <AddFileButton currentFolder={folder} />
