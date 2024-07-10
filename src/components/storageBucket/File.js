37e9986ca246e3c8973cd7faa998d53cb4c28bf5
kulraj chavda <kulraj710@gmail.com>
2024-07-10 16:03:51 +0530

    [feat] : display image preview

--- src/components/storageBucket/File.js
+++ src/components/storageBucket/File.js
@@ -3,14 +3,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 
 export default function File({ file }) {
+  
+  console.log(file)
   return (
     <a
       href={file.url}
       target="_blank"
       className="btn btn-outline-dark text-truncate w-100"
+      data-bs-toggle="tooltip" title={file.name}
     >
+      <div style={{ display : 'flex', flexDirection : 'column', justifyContent : 'space-between'}}>
+      <div style={{ minHeight : '230px'}}>
+      <img src={file.url} alt="No Preview Available" height='200px' width='200px'/>
+      </div>
+
+      <div style={{ fontSize : '12px'}}>
       <FontAwesomeIcon icon={faFile} className="mr-2" />
-      {file.name}
+       <span> {file.name} </span>
+        </div>
+
+      </div>
     </a>
   )
 }
